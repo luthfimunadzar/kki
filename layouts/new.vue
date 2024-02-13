7044e18ba55b59f4e0bdb8592982aa29dd1617dc
Luthfi Munadzar <luthfimunadzar3@gmail.com>
2024-02-13 20:35:12 +0700

    add: contact subscribe

--- layouts/new.vue
+++ layouts/new.vue
@@ -138,90 +138,172 @@
     <v-main>
       <nuxt />
     </v-main>
-    <div class="contact-wrap">
+    <div class="subscribe">
       <v-container>
         <v-row>
-          <v-col md="10" offset-md="1" cols="12" class="pb-0">
-            <h3>Contact Us</h3>
-            <v-row>
-              <v-col cols="12" md="6">
-                <h5>Bandung Home Base</h5>
-                <p>Jl. Taman Radio Dalam Raya no 28, Jakarta Selatan</p>
-                <h5>Say something nice</h5>
-                <a href="mailto:contact@kolaborasi.co"
-                  ><v-icon>far fa-envelope</v-icon> Contact@kolaborasi.co</a
+          <v-col cols="12" sm="12">
+            <div id="mc_embed_shell">
+              <link
+                href="//cdn-images.mailchimp.com/embedcode/classic-061523.css"
+                rel="stylesheet"
+                type="text/css"
+              />
+              <style type="text/css">
+                #mc_embed_signup{background:#fff; false;clear:left; font:14px Helvetica,Arial,sans-serif; width: px;}
+                /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+                  We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+              </style>
+              <div id="mc_embed_signup">
+                <h2>Subscribe now</h2>
+                <form
+                  id="mc-embedded-subscribe-form"
+                  action="https://kolaborasi.us21.list-manage.com/subscribe/post?u=9a6607fc67bd55e6301ed4d89&amp;id=64211357f3&amp;f_id=0018fee6f0"
+                  method="post"
+                  name="mc-embedded-subscribe-form"
+                  class="validate"
+                  target="_blank"
                 >
-                <div class="clearfix"></div>
-                <a href="http://wa.link/pmub9a"
-                  ><v-icon>fab fa-whatsapp</v-icon> 087828469295</a
-                >
-
-                <h5 class="mt-4">Social Media</h5>
-                <a href="" class="socmed mr-2">
-                  <v-icon> fab fa-linkedin </v-icon>
-                </a>
-                <a href="" class="socmed mr-2">
-                  <v-icon> fab fa-instagram </v-icon>
-                </a>
-                <a href="" class="socmed">
-                  <v-icon> fab fa-twitter </v-icon>
-                </a>
-              </v-col>
-              <v-col cols="12" md="6">
-                <h5>Send Us a Message</h5>
-                <validation-observer ref="observer">
-                  <form id="contactForm" @submit.prevent="submit">
-                    <validation-provider
-                      v-slot="{ errors }"
-                      name="Your Name"
-                      rules="required"
+                  <div id="mc_embed_signup_scroll">
+                    <div class="mc-field-group">
+                      <input
+                        id="mce-EMAIL"
+                        type="email"
+                        name="EMAIL"
+                        class="required email"
+                        required=""
+                        value=""
+                        placeholder="youremail@gmail.com"
+                      />
+                      <input
+                        id="mc-embedded-subscribe"
+                        type="submit"
+                        name="subscribe"
+                        class="button"
+                        value="Subscribe"
+                      />
+                    </div>
+                    <div id="mce-responses" class="clear foot">
+                      <div
+                        id="mce-error-response"
+                        class="response"
+                        style="display: none"
+                      ></div>
+                      <div
+                        id="mce-success-response"
+                        class="response"
+                        style="display: none"
+                      ></div>
+                    </div>
+                    <div
+                      aria-hidden="true"
+                      style="position: absolute; left: -5000px"
                     >
-                      <v-text-field
-                        v-model="form.name"
-                        :error-messages="errors"
-                        label="Your Name"
-                        name="name"
-                        required
-                      ></v-text-field>
-                    </validation-provider>
-                    <validation-provider
-                      v-slot="{ errors }"
-                      name="Your Email"
-                      rules="required|email"
-                    >
-                      <v-text-field
-                        v-model="form.email"
-                        :error-messages="errors"
-                        label="Your E-mail"
-                        name="email"
-                        required
-                      ></v-text-field>
-                    </validation-provider>
-                    <validation-provider
-                      v-slot="{ errors }"
-                      name="Your Message"
-                      rules="required"
-                    >
-                      <v-textarea
-                        v-model="form.message"
-                        :error-messages="errors"
-                        label="Your Message"
-                        name="message"
-                        required
-                        auto-grow
-                      ></v-textarea>
-                    </validation-provider>
-                    <button class="btn btn-rounded mt-5" type="submit">
-                      <span>Send Message</span>
-                    </button>
-                  </form>
-                </validation-observer>
-              </v-col>
-            </v-row>
+                      /* real people should not fill this in and expect good
+                      things - do not remove this or risk form bot signups */
+                      <input
+                        type="text"
+                        name="b_9a6607fc67bd55e6301ed4d89_64211357f3"
+                        tabindex="-1"
+                        value=""
+                      />
+                    </div>
+                  </div>
+                </form>
+              </div>
+              <script
+                type="text/javascript"
+                src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"
+              ></script>
+            </div>
+          </v-col>
+          <v-col cols="12" sm="12">
+            <h4>We'll send you 1-2 newsletter per month about :</h4>
+          </v-col>
+          <v-col cols="12" sm="4">
+            <h6>Program Information</h6>
+            <p>
+              We'll inform you about our upcoming Venture Builder program &
+              Venture Service promotion that can be beneficial for your business
+            </p>
+          </v-col>
+          <v-col cols="12" sm="4">
+            <h6>Business & Portfolio Updates</h6>
+            <p>
+              We'll update you on our startup portfolio latest progress &
+              milestone, so you can understand how venture are growing with us
+              over the time
+            </p>
+          </v-col>
+          <v-col cols="12" sm="4">
+            <h6>Program Information</h6>
+            <p>
+              We'll share many upcoming opportunities of collaboration and
+              partnership with our portfolios companies, so you can be part of
+              it
+            </p>
           </v-col>
         </v-row>
       </v-container>
     </div>
+    <footer class="footer-new">
+      <v-container>
+        <v-row>
+          <v-col cols="12" sm="3" md="3">
+            <img src="/kki-white.png" alt="" class="logo" />
+          </v-col>
+          <v-col cols="12" sm="3" md="4">
+            <h3>Contact Us</h3>
+            <p>
+              Jakarta HQ - Jl Taman Radio Dalam Raya no28, Gandaria Utara,
+              Jakarta Selatan 12140
+            </p>
+            <p>
+              Bandung HQ - Komplek Kampung Padi no B11, Dago Pojok, Bandung
+              40135
+            </p>
+          </v-col>
+          <v-col cols="12" sm="3" md="3">
+            <h3>Site Links</h3>
+            <ul>
+              <li>
+                <nuxt-link to="/venture-builder">Venture Builder</nuxt-link>
+              </li>
+              <li>
+                <nuxt-link to="/venture-service">Venture Service</nuxt-link>
+              </li>
+              <li><nuxt-link to="/manifesto">Manifesto </nuxt-link></li>
+              <li><nuxt-link to="/team">Team </nuxt-link></li>
+              <li><nuxt-link to="/portfolio">Portfolios </nuxt-link></li>
+            </ul>
+          </v-col>
+          <v-col cols="12" sm="3" md="2" class="pr-md-0">
+            <h3>Follow Us</h3>
+            <a
+              href="https://www.linkedin.com/company/kolaborasico/mycompany/?viewAsMember=true"
+              target="_blank"
+              class="socmed"
+            >
+              <v-icon> fab fa-linkedin </v-icon>
+            </a>
+            <a
+              href="https://www.instagram.com/kolaborasico/"
+              target="_blank"
+              class="socmed"
+            >
+              <v-icon> fab fa-instagram </v-icon>
+            </a>
+            <a href="mailto:contact@kolaborasi.co" class="socmed"
+              ><v-icon>far fa-envelope</v-icon></a
+            >
+            <a
+              href="https://api.whatsapp.com/send?phone=6287828469295&text=Hello%20Kolaborasi%2C%20saya%20tertarik%20dengan%20..."
+              class="socmed"
+              ><v-icon>fab fa-whatsapp</v-icon></a
+            >
+          </v-col>
+        </v-row>
+      </v-container>
+    </footer>
   </v-app>
 </template>
 
